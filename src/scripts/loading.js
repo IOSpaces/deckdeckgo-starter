c591ec7d213531b5ed89de02ebc83bfce0e88f1e
peterpeterparker <david.dalbusco@outlook.com>
2019-06-06 21:49:48 +0200

    feat: if a parameter screenshot is provided in the url, then all actions and pager will be hidden
    Signed-off-by: peterpeterparker <david.dalbusco@outlook.com>

--- src/scripts/loading.js
+++ src/scripts/loading.js
@@ -1,10 +1,42 @@
 postLoading = () => {
-    return new Promise((resolve) => {
+    return new Promise(async (resolve) => {
         const app = document.querySelector('ion-app');
         if (app) {
             app.classList.remove('loading');
         }
 
+        await initSreenshot();
+
         resolve();
     });
 };
+
+function initSreenshot() {
+    return new Promise((resolve) => {
+        if (!window || !window.location) {
+            resolve();
+            return;
+        }
+
+        const url = new URL(window.location.href);
+        const screenshot = url && url.searchParams ? url.searchParams.has('screenshot') : false;
+
+        if (screenshot) {
+            const navigation = document.querySelector('#navigation');
+
+            if (navigation) {
+                navigation.style.display = 'none';
+            }
+
+            const deck = document.querySelector('deckgo-deck');
+            if (deck) {
+                deck.style.setProperty('--pager-display', 'none');
+
+                const actions = deck.querySelector('[slot="actions"]');
+                if (actions) {
+                    actions.style.display = 'none';
+                }
+            }
+        }
+    });
+}
